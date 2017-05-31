755299ecd1a79c1ebd13eeb61accef8f55adc13a
Cristofer [XPS] <cristofer.dev@gmail.com>
2017-05-30 20:25:38 -0400

    Init
    	new file:   base.js
    	new file:   cristian.png
    	new file:   cristofer.png
    	new file:   index.html
    	new file:   jose.png
    	new file:   luis.png
    	new file:   rodrigo.png

---
+++ base.js
@@ -0,0 +1,37 @@
+function peticion(user){
+
+    var body = null;
+    var xhr = new XMLHttpRequest();
+
+    var url = "https://www.freecodecamp.com/"+user;
+
+    xhr.open("GET", url);
+
+    xhr.onreadystatechange = function(dump){
+      if (this.readyState === 4) {
+        //obj = JSON.parse(this.responseText);
+
+        //console.log(this.responseText);
+        var n = this.responseText.search('\\[ ');
+        var res = this.responseText.substr(n, 7)
+        res = res.replace('[', "");
+        res = res.replace(' ', "");
+        res = res.replace(']', "");
+        res = res.replace('<', "");
+        res = res.replace('>', "");
+        console.log(res);
+        document.getElementById(user).innerHTML = res;
+        
+      }
+    }
+
+
+    xhr.send(body);
+
+}
+
+peticion('rodpoblete');
+peticion('cristofer-dev');
+peticion('jose');
+peticion('c-rojasa');
+peticion('luis');
