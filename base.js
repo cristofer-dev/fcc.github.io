a3f147ce33d8956f19adee08394d125ec47dc76d
Cristofer [XPS] <cristofer.dev@gmail.com>
2017-06-04 17:55:50 -0400

    Update

--- base.js
+++ base.js
@@ -1,17 +1,23 @@
-function peticion(user){
+var objRanking = {};
+var max = 0;
+var arrayUser = [
+    'rodpoblete',
+    'cristofer-dev',
+    'jakojazz',
+    'c-rojasa',
+    'luis'
+];
+
+function peticion(user,callback){
 
     var body = null;
     var xhr = new XMLHttpRequest();
-
     var url = "https://www.freecodecamp.com/"+user;
 
     xhr.open("GET", url);
 
     xhr.onreadystatechange = function(dump){
       if (this.readyState === 4) {
-        //obj = JSON.parse(this.responseText);
-
-        //console.log(this.responseText);
         var n = this.responseText.search('\\[ ');
         var res = this.responseText.substr(n, 7)
         res = res.replace('[', "");
@@ -19,19 +25,50 @@ function peticion(user){
         res = res.replace(']', "");
         res = res.replace('<', "");
         res = res.replace('>', "");
-        console.log(res);
-        document.getElementById(user).innerHTML = res;
-        
+        //console.log(res);
+        res = parseInt(res);
+        res = (isNaN(res)) ? 0 : res ;
+        document.getElementById(user).innerHTML = res + " Km/h";
+        callback(null,res,user)
       }
     }
-
-
     xhr.send(body);
+}
+
+function parse(arrayUser){
+    var arrPromise = [];
+
+    for (i = 0; i < arrayUser.length; i++) {
+        const lambdapromisificado = new Promise(function(resolve, reject) {
+            
+            peticion(arrayUser[i],function(err,puntos,participante){
+                objRanking[participante] = puntos;
+                max = (parseInt(puntos) > max) ? puntos : max;
+                
+            resolve();  
+            });
+        
+        });//promise
+    arrPromise.push(lambdapromisificado);
+    }//
+
+    Promise.all(arrPromise).then(function() {
+        for (i = 0; i < arrayUser.length; i++) {
+            
+            var puntos = parseInt(objRanking[arrayUser[i]]);
+
+            var vh = 80 - ( puntos * 80 ) / max ;
+            
+            vh = (isNaN(vh)) ? 80 : vh;
+            console.log(vh);
+
+            document.getElementById(arrayUser[i]+"-col").setAttribute("style", "padding-top: "+vh+"vh;");
+        }//for
+
+        
+        console.log("Yeaaa " + max);
+    });
 
 }
 
-peticion('rodpoblete');
-peticion('cristofer-dev');
-peticion('jakojazz');
-peticion('c-rojasa');
-peticion('luis');
+parse(arrayUser);
